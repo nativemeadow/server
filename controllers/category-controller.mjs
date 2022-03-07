65af04e0c9656d37e172910dac1a95272776dfca
Joe McClintock <joseph@nativemeadow.com>
2022-03-07 11:10:22 -0800

    added api for returning categories and products as JSON

--- controllers/category-controller.mjs
+++ controllers/category-controller.mjs
@@ -2,11 +2,41 @@ import httpError from '../models/http-error.mjs';
 import DbService from '../config/db.mjs';
 import { response } from 'express';
 
+export const getHierarchy = async (req, res, next) => {
+    const { id } = req.params;
+    const db = DbService.getDbServiceInstance();
+    const query = `
+        WITH RECURSIVE
+        ancestors(parent_category, url_key, id, title, path)
+        AS
+            (SELECT parent_category, url_key, id, title, CAST(CONCAT('{"id": ', id, ', "title": ', '"', title, '"}') as CHAR(700))  
+            FROM category WHERE parent_category IS NULL
+            UNION ALL
+            SELECT category.parent_category, category.url_key, category.id, category.title,
+                CONCAT(ancestors.path, ', ', CONCAT('{"id": ', category.id, ', "title": ', '"', category.title, '"}'))
+            FROM ancestors JOIN category ON ancestors.id = category.parent_category
+            )
+        SELECT parent_category, url_key, id, title, CONCAT('[', path, ']') as path FROM ancestors WHERE id = ? ORDER BY parent_category, id;
+    `
+
+    const result = db.getCategoryData(query, [id]);
+
+    result
+        .then(data => {
+            const jsonData = JSON.parse(data[0].path);
+            console.log(jsonData);
+            res.json(jsonData);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+
+}
+
 export const getCategories = async (req, res, next) => {
     const { id } = req.params;
     const db = DbService.getDbServiceInstance();
-    const parentId = id || ' is null';
-    const query = 'SELECT id, title, url_key, image, category_order from category where parent_category' + parentId + ' order by category_order';
+    const query = 'SELECT id, title, url_key, image, category_order from category where parent_category is null order by category_order';
 
     const result = db.getCategoryData(query);
 
@@ -29,6 +59,91 @@ export const getCategory = async (req, res, next) => {
     result
         .then(data => {
             res.json({ data });
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+export const getCategoryProducts = async (req, res, next) => {
+    const { id } = req.params;
+    const db = DbService.getDbServiceInstance();
+    const query = `SELECT product.id, product.sku, product.title, product.description, product.image
+        FROM product    
+        JOIN product_category ON product_category.product = product.id
+        JOIN category ON product_category.category = category.id
+        WHERE category.id = ?`
+
+    const result = db.getCategoryData(query, [id]);
+
+    result
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+export async function getProductPricing(req, res, next) {
+    const { id } = req.params;
+    const db = DbService.getDbServiceInstance();
+    const query = `SELECT price.* FROM price where price.product = ?`;
+
+    const result = db.getCategoryData(query, [id]);
+
+    result.then(data => {
+        res.json(data);
+    })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+export const getProductsPerCategory = async (req, res, next) => {
+    const { id } = req.params;
+    const db = DbService.getDbServiceInstance();
+    const query = `SELECT product.id, product.sku, product.title, product.description, product.image,
+            price.title as priceTitle, price.description as priceDescr, price.price, price.size, price.units, price.coverage, price.coverage_value 
+        FROM product 
+        JOIN price ON price.product = product.id   
+        JOIN product_category ON product_category.product = product.id
+        JOIN category ON product_category.category = category.id
+        WHERE category.id = ?`
+
+    const result = db.getCategoryData(query, [id]);
+
+    result
+        .then(data => {
+            const productArray = [];
+            let productObject = {};
+            let pricingObject = {};
+            data.forEach((prod, index, prodArray) => {
+                if (index === 0 || prod.id !== prodArray[index - 1].id) {
+                    productObject = {
+                        id: prod.id,
+                        sku: prod.sku,
+                        title: prod.title,
+                        description: prod.description,
+                        image: prod.image,
+                        pricing: [],
+                    };
+                    productArray.push(productObject);
+                }
+                pricingObject = {
+                    title: prod.priceTitle,
+                    description: prod.priceDescr,
+                    price: prod.price,
+                    size: prod.size,
+                    units: prod.units,
+                    coverage: prod.coverage,
+                    coverage_value: prod.coverage_value
+                };
+                productObject.pricing.push(pricingObject);
+            });
+
+            res.json(productArray);
         })
         .catch(err => {
             console.error(err);
--- routes/category-routes.mjs
+++ routes/category-routes.mjs
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getCategories, getCategory } from '../controllers/category-controller.mjs';
+import { getCategories, getCategory, getHierarchy, getCategoryProducts, getProductPricing, getProductsPerCategory } from '../controllers/category-controller.mjs';
 
 const router = express.Router();
 
@@ -13,5 +13,24 @@ router.get('/:id', (req, res, next) => {
     getCategory(req, res, next);
 });
 
+router.get('/hierarchy/:id', (req, res, next) => {
+    console.log('get parent categories');
+    getHierarchy(req, res, next);
+});
+
+router.get('/products/:id', (req, res, next) => {
+    console.log('get products');
+    getCategoryProducts(req, res, next);
+});
+
+router.get('/pricing/:id', (req, res, next) => {
+    console.log('get products');
+    getProductPricing(req, res, next);
+});
+
+router.get('/product-pricing/:id', (req, res, next) => {
+    console.log('get product pricing');
+    getProductsPerCategory(req, res, next);
+});
 
 export default router;
